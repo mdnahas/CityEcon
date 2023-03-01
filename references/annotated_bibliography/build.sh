22933ea02490e286ba37c0fef56f10c9c6a29c18
Michael Nahas <mike@nahas.com>
2023-03-01 09:07:34 -0600

    Updated annotated bibliography

--- references/annotated_bibliography/build.sh
+++ references/annotated_bibliography/build.sh
@@ -3,5 +3,9 @@
 htlatex annotated_bibliography
 bibtex annotated_bibliography
 htlatex annotated_bibliography
+
+# This makes ids match the Bibtex names like "Fischer1995"
+sed -i 's/id="X/id="/g' annotated_bibliography.html
+
 mv annotated_bibliography.html annotated_bibliography.css ..
 
